600348fb1d32aeb178fac6c268352eae86781b5d
Nahid <amirul.islam@vivasoftltd.com>
2025-06-24 13:15:48 +0600

    c-4 Serve Static Assets

--- myApp.js
+++ myApp.js
@@ -1,6 +1,8 @@
 let express = require('express');
 let app = express();
 
+// We have set '/public' as the static directory in the 'index.html'
+app.use('/public', express.static(__dirname + '/public'));
 
 const home_html = __dirname + '/views/index.html';
 
