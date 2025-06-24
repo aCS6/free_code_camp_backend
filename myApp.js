b580041a2aeb0e5dcf82302c68db0deb8e84d00d
Nahid <amirul.islam@vivasoftltd.com>
2025-06-24 17:44:53 +0600

    c-10 Get Query Parameter Input from the Client

--- myApp.js
+++ myApp.js
@@ -1,79 +1,67 @@
-let express = require('express');
-require('dotenv').config()
+let express = require("express");
+require("dotenv").config();
 
 let app = express();
 
 app.use(function (req, res, next) {
-    let _log = `${req.method} ${req.path} - ${req.ip}`
-    console.log(_log);
-    next();
-})
+  let _log = `${req.method} ${req.path} - ${req.ip}`;
+  console.log(_log);
+  next();
+});
 // We have set '/public' as the static directory in the 'index.html'
-app.use('/public', express.static(__dirname + '/public'));
+app.use("/public", express.static(__dirname + "/public"));
 
-const home_html = __dirname + '/views/index.html';
+const home_html = __dirname + "/views/index.html";
 
-app.get('/', function (req, res) {
-    res.sendFile(home_html);
-})
+app.get("/", function (req, res) {
+  res.sendFile(home_html);
+});
 
+app.get("/json", function (req, res) {
+  const _case = process.env.MESSAGE_STYLE;
 
-app.get('/json', function (req, res) {
-    const _case = process.env.MESSAGE_STYLE
-    
-    let message = 'Hello json';
+  let message = "Hello json";
 
-    if (_case === 'uppercase') {
-        message = message.toUpperCase();
-    }
+  if (_case === "uppercase") {
+    message = message.toUpperCase();
+  }
 
-    res.json({
-        message: message
-    });
-})
+  res.json({
+    message: message,
+  });
+});
 
-
-app.get('/now', function (req, res, next) {
+app.get(
+  "/now",
+  function (req, res, next) {
     // Attach the current datetime with the request object
     req.time = new Date().toString();
     next();
-}, function (req, res) {
+  },
+  function (req, res) {
     res.json({
-        time: req.time
+      time: req.time,
     });
-})
+  }
+);
 
+app.get("/:word/echo", function (req, res) {
+  res.json({
+    echo: req.params.word,
+  });
+});
 
-app.get('/:word/echo', function (req, res) {
+app
+  .route("/name")
+  .get(function (req, res) {
     res.json({
-        echo: req.params.word
+      name: req.query.first + " " + req.query.last,
     });
-})
+  })
+  .post(function (req, res) {
+    res.json({
+      name: req.body.first + " " + req.body.last,
+    });
+  });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
